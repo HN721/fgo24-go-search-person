b694fb5d468ff5045c3dfa477ea0b6d17dfb88a8
HN721 <hosea1422@gmail.com>
2025-06-09 15:11:37 +0700

    feat: create func to search on array

---
+++ main.go
@@ -0,0 +1,33 @@
+package main
+
+import "fmt"
+
+func main() {
+	users := []string{
+		"Leanne Graham",
+		"Ervin Howell",
+		"Clementine Bauch",
+		"Patricia Lebsack",
+		"Chelsey Dietrich",
+		"Mrs. Dennis Schulist",
+		"Kurtis Weissnat",
+		"Nicholas Runofsdottir V",
+		"Glena Reichert",
+		"Clementina DuBuque",
+	}
+	searchParams(users, "Glena Reichert")
+}
+func searchParams(users []string, name string) {
+	for x := range users {
+		if users[x] == name {
+			result := []string{users[x]}
+
+			fmt.Println(result)
+		} else {
+			result := []string{}
+			fmt.Println(result)
+
+		}
+
+	}
+}
